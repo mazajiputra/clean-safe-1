83eca7662c1cc311724d07e81b1b89e123ed7895
mazajiputra <mazajiputra@gmail.com>
2022-05-29 00:28:44 +0700

    Update sensor_raw.py

--- sensor_raw.py
+++ sensor_raw.py
@@ -29,11 +29,9 @@ def baca_s(no_sensor):
     finally:
         #2_Perangkaian data
         global data_full
-        data_suhu = {f's{no_sensor}_suhu': temperature}
-        data_kelembaban = {f's{no_sensor}_kelembaban': relative_humidity}
+        data = {f's{no_sensor}_suhu': temperature, f's{no_sensor}_kelembaban': relative_humidity}
         #3_Pengappen data ke data full
-        data_full=np.append(data_full, data_suhu)
-        data_full=np.append(data_full, data_kelembaban)
+        data_full=data_full+data
         # print(data_suhu,data_kelembaban)
         print(data_full)
         
