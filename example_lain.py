d0c9eb21c4ec95a1ffcbfdfa20580dc331288386
mazajiputra <mazajiputra@gmail.com>
2022-05-28 13:34:35 +0700

    Create example_lain.py

---
+++ example_lain.py
@@ -0,0 +1,10 @@
+import busio
+import time
+
+import adafruit_shtc3
+
+i2c = busio.I2C(board.SCL, board.SDA)
+sht = adafruit_shtc3.SHTC3(i2c)
+while True:
+    print(sht.temperature)
+    time.sleep(1)
