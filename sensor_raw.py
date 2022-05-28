9d8805e24aef6ce14ab5a0e949f4a8f7533227c0
mazajiputra <mazajiputra@gmail.com>
2022-05-29 00:16:57 +0700

    Update sensor_raw.py

--- sensor_raw.py
+++ sensor_raw.py
@@ -35,7 +35,7 @@ def baca_s(no_sensor):
         }
         #3_Pengappen data ke data full
 
-        data_full=np.concatenate(data_full, data)
+        data_full=np.concatenate((data_full, data), axis=None)
 
         print(data)
         print(data_full)
