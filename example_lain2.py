a3594c71e49df6046e258d5c9b942d0c4bc5be59
mazajiputra <mazajiputra@gmail.com>
2022-05-29 18:33:40 +0700

    Update example_lain2.py

--- example_lain2.py
+++ example_lain2.py
@@ -1,35 +1,27 @@
-from struct import pack
+from struct import unpack
 from sensirion_i2c_driver import LinuxI2cTransceiver, I2cConnection, \
     I2cDevice, SensirionI2cCommand, CrcCalculator
+from sensirion_i2c_sht.sht3x import Sht3xTemperature, Sht3xHumidity
 
 
-# Implement a command
-class MyI2cCmdReadSerialNumber(SensirionI2cCommand):
+class Shtc3I2cCmdMeasure(SensirionI2cCommand):
     def __init__(self):
-        super(MyI2cCmdReadSerialNumber, self).__init__(
-            command=0xD033,
+        super(Shtc3I2cCmdMeasure, self).__init__(
+            command=0x7866,
             tx_data=[],
-            rx_length=48,
-            read_delay=0,
+            rx_length=6,
+            read_delay=0.02,
             timeout=0,
             crc=CrcCalculator(8, 0x31, 0xFF),
         )
 
     def interpret_response(self, data):
-        raw_response = SensirionI2cCommand.interpret_response(self, data)
-        return str(raw_response.decode('utf-8').rstrip('\0'))
+        checked_data = SensirionI2cCommand.interpret_response(self, data)
+        temperature_ticks, humidity_ticks = unpack(">2H", checked_data)
+        return Sht3xTemperature(temperature_ticks), Sht3xHumidity(humidity_ticks)
 
 
-# Implement a device
-class MyI2cDevice(I2cDevice):
-    def __init__(self, connection, slave_address=0x69):
-        super(MyI2cDevice, self).__init__(connection, slave_address)
-
-    def read_serial_number(self):
-        return self.execute(MyI2cCmdReadSerialNumber())
-
-
-# Usage
 with LinuxI2cTransceiver('/dev/i2c-1') as transceiver:
-    device = MyI2cDevice(I2cConnection(transceiver))
-    print("Serial Number: {}".format(device.read_serial_number()))
+    device = I2cDevice(I2cConnection(transceiver), 0x70)
+    temperature, humidity = device.execute(Shtc3I2cCmdMeasure())
+    print("Temperature: {}, Humidity: {}".format(temperature, humidity))
