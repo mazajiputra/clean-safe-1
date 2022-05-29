cf820d436d80e49acd6e38dc38af4e34fdade13a
mazajiputra <mazajiputra@gmail.com>
2022-05-29 19:05:45 +0700

    Update sensor_raw.py

--- sensor_raw.py
+++ sensor_raw.py
@@ -6,6 +6,35 @@ import pandas as pd
 from datetime import datetime
 import numpy as np 
 from array import array
+from struct import unpack
+from sensirion_i2c_driver import LinuxI2cTransceiver, I2cConnection, \
+    I2cDevice, SensirionI2cCommand, CrcCalculator
+from sensirion_i2c_sht.sht3x import Sht3xTemperature, Sht3xHumidity
+
+class Shtc3I2cCmdMeasure(SensirionI2cCommand):
+    def __init__(self):
+        super(Shtc3I2cCmdMeasure, self).__init__(
+            command=0x7866,
+            tx_data=[],
+            rx_length=6,
+            read_delay=0.02,
+            timeout=0,
+            crc=CrcCalculator(8, 0x31, 0xFF),
+        )
+
+    def interpret_response(self, data):
+        checked_data = SensirionI2cCommand.interpret_response(self, data)
+        temperature_ticks, humidity_ticks = unpack(">2H", checked_data)
+        return Sht3xTemperature(temperature_ticks), Sht3xHumidity(humidity_ticks)
+
+def cetak():
+    with LinuxI2cTransceiver('/dev/i2c-1') as transceiver:
+        device = I2cDevice(I2cConnection(transceiver), 0x70)
+        temperature, humidity = device.execute(Shtc3I2cCmdMeasure())
+        print("Temperature: {}, Humidity: {}".format(temperature, humidity))
+        return temperature,humidity
+
+
 def waktu_now(data_full):
     waktu=datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     data_full={'waktu': waktu}
@@ -15,19 +44,11 @@ def waktu_now(data_full):
 def baca_s(no_sensor,data_full):
     print(data_full)
     try:
-        time.sleep(5)
-        i2c = board.I2C()   # uses board.SCL and board.SDA
-        sht = adafruit_shtc3.SHTC3(i2c)
-        temperature, relative_humidity = sht.measurements
-        # print("Temperature: %0.1f C" % temperature)
-        # print("Humidity: %0.1f %%" % relative_humidity)
-        print(temperature)
-        print(relative_humidity)
-        print("")      
-    
+        temperature,humidity=cetak()
+        
     except Exception:
         temperature=0
-        relative_humidity=0
+        humidity=0
     finally:
         #2_Perangkaian data
         # key='s'+str(no_sensor)+'_suhu'
@@ -38,7 +59,7 @@ def baca_s(no_sensor,data_full):
         # data_full[f's{no_sensor}_kelembaban'] = relative_humidity
         # print(data_suhu,data_kelembaban)
         
-        data={f's{no_sensor}_suhu': temperature,f's{no_sensor}_kelembaban' : relative_humidity}
+        data={f's{no_sensor}_suhu': temperature,f's{no_sensor}_kelembaban' : humidity}
         data_full=Merge(data_full,data)
         print(data_full)
         return data_full
