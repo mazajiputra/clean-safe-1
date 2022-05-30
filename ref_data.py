4ad15f3528fbe79e8f6cf12e157bcf24e36bfb57
mazajiputra <mazajiputra@gmail.com>
2022-05-31 00:38:35 +0700

    Update ref_data.py

--- ref_data.py
+++ ref_data.py
@@ -6,8 +6,8 @@ def simpan_dt(df_baru):
     #Mengecek file yg lama
     file = 'data/file_data.csv'
     if(os.path.exists(file) and os.path.isfile(file)):
-        # os.remove(file)
-        # print("file deleted")
+        os.remove(file)
+        print("file deleted")
         #Bila file nya ada, tambah data
         #1. data di file dibaca
         df_lama=pd.read_csv('data/file_data.csv')
@@ -15,11 +15,11 @@ def simpan_dt(df_baru):
         df_mix=pd.concat([df_lama, df_baru],ignore_index=True,sort=False)
         print(df_mix.head())
         #3 data lengkap sudah siap, Masukkan ke file lagi
-        df_mix.to_csv('data/file_data.csv', index=True)
+        df_mix.to_csv('data/file_data.csv', index=False)
         print("File berhasil diperbarui")
         
 
     else:
         print("file not found, make again")
 
-        df_baru.to_csv('data/file_data.csv', index=True)
+        df_baru.to_csv('data/file_data.csv', index=False)
