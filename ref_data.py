a9d028d1404b0554c8c7d2c25dfd113e7fa991dd
mazajiputra <mazajiputra@gmail.com>
2022-05-31 00:37:02 +0700

    Update ref_data.py

--- ref_data.py
+++ ref_data.py
@@ -12,7 +12,7 @@ def simpan_dt(df_baru):
         #1. data di file dibaca
         df_lama=pd.read_csv('data/file_data.csv')
         #2. data dari file + data baru dimix
-        df_mix=pd.concat([df_lama, df_baru],ignore_index=True)
+        df_mix=pd.concat([df_lama, df_baru],ignore_index=True,sort=False)
         print(df_mix.head())
         #3 data lengkap sudah siap, Masukkan ke file lagi
         df_mix.to_csv('data/file_data.csv', index=True)
