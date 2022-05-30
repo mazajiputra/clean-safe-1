3df3dab4a925ec9914834a4b3adc753189f59fc5
mazajiputra <mazajiputra@gmail.com>
2022-05-30 21:49:27 +0700

    Update sensor_raw.py

--- sensor_raw.py
+++ sensor_raw.py
@@ -64,14 +64,9 @@ def baca_s(no_sensor,data_full):
         print(temperature)
         print(humidity)
         #2. Data dibuat data frame
-        # 'df_{}'.format(keys)
-        data_s=copy(data_full)
-        data_s.insert(0,'s{}_suhu'.format(no_sensor), temperature )
-        # data_full.assign('s{}_kelembaban'.format(no_sensor)= humidity )
-
-        # data_full['s{}_suhu'.format(no_sensor)] = temperature
-        #3. Data digabungkan dengan data yg sebelumnya
-        print(data_s)
+        d={f's{no_sensor}_suhu':[temperature],f's{no_sensor}_kelembaban':[humidity]}
+        data_sensor=pd.DataFrame(data=d)
+        print(data_sensor)
 
         return data_s
         #Backup
