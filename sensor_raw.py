7954d6b7b703ad74260e08cf68eb101bd2c7701b
mazajiputra <mazajiputra@gmail.com>
2022-05-30 14:49:31 +0700

    Update sensor_raw.py

--- sensor_raw.py
+++ sensor_raw.py
@@ -32,7 +32,7 @@ def cetak():
     with LinuxI2cTransceiver('/dev/i2c-1') as transceiver:
         device = I2cDevice(I2cConnection(transceiver), 0x70)
         response = device.execute(Shtc3I2cCmdMeasure())
-        print("Interval: {} ms".format(response.measure_interval_ms))
+        #print("Interval: {} ms".format(response.measure_interval_ms))
         print("Temperature outside: {} °C".format(response.temperature_outside.degree_celsius))
         print("Temperature inside: {}".format(response.temperature_inside))
         temperature,humidity=response
