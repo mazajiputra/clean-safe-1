c392bb4473f13e38cd45df7f48472e3bddc185d7
mazajiputra <mazajiputra@gmail.com>
2022-05-31 00:32:59 +0700

    Update ref_data.py

--- ref_data.py
+++ ref_data.py
@@ -8,54 +8,18 @@ def simpan_dt(df_baru):
     if(os.path.exists(file) and os.path.isfile(file)):
         # os.remove(file)
         # print("file deleted")
-        #Bila file nya ada tambah
+        #Bila file nya ada, tambah data
+        #1. data di file dibaca
         df_lama=pd.read_csv('data/file_data.csv')
+        #2. data dari file + data baru dimix
         df_mix=pd.concat([df_lama, df_baru],ignore_index=True)
-        print(df_mix)
-
-        #Masukkan ke file lagi
+        print(df_check.head())
+        #3 data lengkap sudah siap, Masukkan ke file lagi
         df_mix.to_csv('data/file_data.csv', index=True)
+        print("File berhasil diperbarui")
+        
 
     else:
         print("file not found, make again")
 
         df_baru.to_csv('data/file_data.csv', index=True)
-
-    # #create DataFrame
-    # df0 = pd.DataFrame({'waktu': [0],
-    #                 's1_suhu': [0],
-    #                 's1_kelembaban': [0],
-    #                 's2_suhu': [0],
-    #                 's2_kelembaban': [0],
-    #                 's3_suhu': [0],
-    #                 's3_kelembaban': [0],
-    #                 's4_suhu': [0],
-    #                 's4_kelembaban': [0],
-    #                 })
-
-
-
-
-
-
-
-
-
-    #view DataFrame
-
-
-    # print (df0)
-
-    #export DataFrame to CSV file
-    df0.to_csv('data/file_data.csv', index=True)
-
-
-
-    #Gabungkan kedua data frame
-    df_mix=pd.concat([df_lama, df_baru],ignore_index=True)
-
-    df0.to_csv('data/file_data.csv', index=True)
-
-
-    df_check=pd.read_csv('data/file_data.csv')
-    print(df_check.head())
