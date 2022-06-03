5053e24e1e205e82428f6a52b937d1b0acd72620
mazajiputra <mazajiputra@gmail.com>
2022-06-03 18:10:26 +0700

    Update sensor_raw.py

--- sensor_raw.py
+++ sensor_raw.py
@@ -24,6 +24,8 @@ class Shtc3I2cCmdMeasure(SensirionI2cCommand):
             timeout=0,
             crc=CrcCalculator(8, 0x31, 0xFF),
         )
+        return self.ticks
+    
 
     def interpret_response(self, data):
         checked_data = SensirionI2cCommand.interpret_response(self, data)
